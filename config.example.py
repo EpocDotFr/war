2535a90261e4d1e8196884d9bf5601ba730cfe9a
Epoc <contact@epoc.fr>
2016-06-14 23:35:50 +0200

    Test Chromaprint

--- config.example.py
+++ config.example.py
@@ -1,5 +1,5 @@
-TIMEZONE='Europe/Paris'
-SECRET_KEY='secretkeyhere'
-SESSION_COOKIE_NAME='WAR'
-DEBUG=False
-SAMPLES_PATH='storage/samples/'
+TIMEZONE = 'Europe/Paris'
+SECRET_KEY = 'secretkeyhere'
+SESSION_COOKIE_NAME = 'WAR'
+DEBUG = False
+SAMPLES_PATH = 'storage/samples/'
--- war.py
+++ war.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, jsonify, request, abort
-import os, uuid
+import os
+import uuid
+import acoustid
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+
 
 # Home page with the Recognize button
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 # Receive and handle the sample WAV file
 @app.route('/sample', methods=['POST'])
@@ -19,27 +23,51 @@ def sample():
 
     if not request.is_xhr or not request.files['file']:
         result['data']['message'] = 'Bad request.'
-        abort(400, result)
+        abort(400, result)  # TODO improve this
 
     try:
         sample_file = request.files['file']
 
-        sample_file_destination = os.path.abspath(app.config['SAMPLES_PATH'])
-        sample_file_name = '{}.wav'.format(uuid.uuid4())
+        sample_file_uuid = uuid.uuid4()
 
-        sample_file.save(os.path.join(sample_file_destination, sample_file_name))
+        sample_file.save(get_sample_file_path(sample_file_uuid))
+
+        # TODO everything after this line is tmeporary
+
+        generate_fingerprint(sample_file_uuid)
+
+        # TODO end temporary
     except Exception as e:
         result['data']['message'] = str(e)
-        abort(500, result)
+        abort(500, result)  # TODO improve this
 
     return jsonify(result)
+
 
 # Not Found
 @app.errorhandler(404)
 def error_404(error):
     return render_template('404.html', error=error)
 
+
 # Internal Server Error
 @app.errorhandler(500)
 def error_500(error):
     return render_template('500.html', error=error)
+
+
+def get_sample_file_path(sample_file_uuid):
+    sample_file_destination = os.path.abspath(app.config['SAMPLES_PATH'])
+    sample_file_name = '{}.wav'.format(sample_file_uuid)
+    sample_file_path = os.path.join(sample_file_destination, sample_file_name)
+
+    return sample_file_path
+
+
+def generate_fingerprint(sample_file_uuid):
+    sample_file_path = get_sample_file_path(sample_file_uuid)
+
+    if not os.path.exists(sample_file_path):
+        raise Exception('The sample file does not exists anymore.')
+
+    print(acoustid.fingerprint_file(sample_file_path))
