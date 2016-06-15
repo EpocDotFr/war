452465de2dc477b499abecb779a4141f7c7ab360
Epoc <contact@epoc.fr>
2016-06-15 13:51:45 +0200

    Implement AcoutID match and logging

--- config.example.py
+++ config.example.py
@@ -3,3 +3,7 @@ SECRET_KEY = 'secretkeyhere'
 SESSION_COOKIE_NAME = 'WAR'
 DEBUG = False
 SAMPLES_PATH = 'storage/samples/'
+LOGS_PATH = 'storage/logs/'
+ACOUSTID_API_KEY = 'apikeyhere'
+SAMPLE_DURATION = 5
+LOGGER_HANDLER_POLICY = 'production'
--- war.py
+++ war.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, jsonify, request, abort
+from logging.handlers import RotatingFileHandler
 import os
 import uuid
 import acoustid
+import logging
+
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+
+logging_handler = RotatingFileHandler(os.path.join(app.config['LOGS_PATH'], 'war.log'), maxBytes=10000, backupCount=2)
+logging_handler.setLevel(logging.INFO)
+logging_handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s"))
+
+app.logger.addHandler(logging_handler)
 
 
 # Home page with the Recognize button
@@ -20,26 +29,37 @@ def sample():
         'result': 'failure',
         'data': {}
     }
+    
+    app.logger.info('New request')
 
     if not request.is_xhr or not request.files['file']:
-        result['data']['message'] = 'Bad request.'
-        abort(400, result)  # FIXME improve this to return proper JSON
+        app.logger.error('Not an Ajax request or no sample')
+        result['data']['message'] = 'Invalid request'
 
     try:
         sample_file = request.files['file']
 
         sample_file_uuid = uuid.uuid4()
+        
+        sample_file_path = get_sample_file_path(sample_file_uuid)
+        
+        app.logger.info('Saving sample file to {}'.format(sample_file_path))
 
-        sample_file.save(get_sample_file_path(sample_file_uuid))
+        sample_file.save(sample_file_path)
+        
+        result['result'] = 'success'
+        result['data']['uuid'] = sample_file_uuid
 
         # TODO everything after this line is temporary
 
-        generate_fingerprint(sample_file_uuid)
+        recognize_results = recognize(sample_file_uuid)
+        
+        result['data']['recognize_results'] = recognize_results
 
         # TODO end temporary
     except Exception as e:
+        app.logger.error(e)
         result['data']['message'] = str(e)
-        abort(500, result)  # FIXME improve this to return proper JSON
 
     return jsonify(result)
 
@@ -47,12 +67,14 @@ def sample():
 # Not Found
 @app.errorhandler(404)
 def error_404(error):
+    app.logger.warning(error)
     return render_template('404.html', error=error)
 
 
 # Internal Server Error
 @app.errorhandler(500)
 def error_500(error):
+    app.logger.error(error)
     return render_template('500.html', error=error)
 
 # TODO put all functions bellow in a module
@@ -65,10 +87,22 @@ def get_sample_file_path(sample_file_uuid):
     return sample_file_path
 
 
-def generate_fingerprint(sample_file_uuid):
+def recognize(sample_file_uuid):
     sample_file_path = get_sample_file_path(sample_file_uuid)
 
     if not os.path.exists(sample_file_path):
-        raise Exception('The sample file does not exists anymore.')
-
-    print(acoustid.fingerprint_file(sample_file_path))  # TODO temporary
+        raise Exception('The sample file does not exists')
+    
+    app.logger.info('Fingerprinting {}'.format(sample_file_path))
+    
+    fingerprint = acoustid.fingerprint_file(sample_file_path)
+    
+    app.logger.info('Result: {}'.format(fingerprint[1]))
+    app.logger.info('Sending match request to AcoustID');
+    
+    lookup_results = acoustid.lookup(app.config['ACOUSTID_API_KEY'], fingerprint[1], fingerprint[0])
+    
+    if not lookup_results['status'] or lookup_results['status'] != 'ok':
+        raise Exception(lookup_results['message'])
+    
+    return lookup_results['results']
