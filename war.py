3e6e0a25b05d05f6965fa71f93d7fd5828457714
Epoc <contact@epoc.fr>
2016-07-10 17:45:24 +0200

    Add 401 and 403 error pages

--- war.py
+++ war.py
@@ -175,3 +175,15 @@ def error_500(error):
 @app.errorhandler(503)
 def error_503(error):
     return render_template('503.html'), 503
+
+
+# Unauthorized
+@app.errorhandler(401)
+def error_401(error):
+    return render_template('401.html'), 401
+
+
+# Forbidden
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('403.html'), 403
