991d7491aa174530f9e9ebc139165dd5e9ed588d
Epoc <contact@epoc.fr>
2016-07-12 13:52:10 +0200

    Thing

--- war.py
+++ war.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, g
 from logging.handlers import RotatingFileHandler
 import os
 import logging
